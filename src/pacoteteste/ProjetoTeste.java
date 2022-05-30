681abba49338c87314ee656296c667cf0a1e182a
git-silvia <silviaaquino@msn.com>
2022-05-30 17:23:28 -0300

    Teste de Commit pelo Eclipse.

---
+++ src/pacoteteste/ProjetoTeste.java
@@ -0,0 +1,12 @@
+package pacoteteste;
+
+public class ProjetoTeste {
+	public static void main(String[] args) {
+		int a = 2;
+
+		int b = 3;
+		System.out.println("Hello Silvia no Eclipse " + (a + b));
+
+	}
+
+}
