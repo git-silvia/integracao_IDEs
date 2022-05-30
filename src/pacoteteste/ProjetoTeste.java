85c06457d657e6bc5e39967b5a803f774d4879d9
git-silvia <silviaaquino@msn.com>
2022-05-30 17:47:23 -0300

    Commit a partir do Eclipse mais tentativas com uma nova modificação.

--- src/pacoteteste/ProjetoTeste.java
+++ src/pacoteteste/ProjetoTeste.java
@@ -2,7 +2,7 @@ package pacoteteste;
 
 public class ProjetoTeste {
 	public static void main(String[] args) {
-		int a = 2;
+		int a = 1;
 
 		int b = 3;
 		System.out.println("Hello Silvia no Eclipse " + (a + b));
